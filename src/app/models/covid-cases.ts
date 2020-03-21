6eb6b8c7e79eb3751730f10cf4c58f05a39c724b
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-21 18:56:08 +0100

    CovidCases interface

---
+++ src/app/models/covid-cases.ts
@@ -0,0 +1,14 @@
+export interface CovidCases {
+  id: string;
+  country_name: string;
+  total_cases: string;
+  new_cases: string;
+  active_cases: string;
+  total_deaths: string;
+  new_deaths: string;
+  total_recovered: string;
+  serious_critical: string;
+  region: string;
+  total_cases_per1m: string;
+  record_date: string;
+}
