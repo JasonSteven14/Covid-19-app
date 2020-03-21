e63fa492ac252310b6e012f7aeeb83327f77e331
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-21 18:55:43 +0100

    Home page added covid helper

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Covid19HelperService } from '../services/covid-19-helper.service';
+import { CovidCases } from '../models/covid-cases';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  cases: CovidCases;
 
+  constructor(
+    private covid19Helper: Covid19HelperService
+  ) { }
+
+  ngOnInit() {
+    this.covid19Helper.getCovid19CasesByCountrySpain().then(
+      res => {
+        this.cases = res;
+      }
+    );
+  }
+  refreshCases() {
+  }
 }
