9034b877c64bdabf6995ee5cb954362d7d34545d
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-25 23:06:17 +0100

    Added method getCasesByCountry

--- src/app/covid-19.service.ts
+++ src/app/covid-19.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CovidCases } from './models/covid-cases';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +7,16 @@ export class Covid19Service {
 
   constructor() { }
 
-  getCasesByCountrySpain(): Promise<CovidCases[]> {
-    const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php?country=spain';
-    const data: Promise<CovidCases[]> = fetch(url, {
+  getCasesByCountrySpain() {
+    const url = 'https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=SPAIN';
+    const data = fetch(url, {
       method: 'GET',
       headers: {
         'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
         'x-rapidapi-key': 'fc9b700d8cmsh07c4965a20c35efp18bd1ajsn0bb779b30d62'
       }
-    }).then(response => {
-      const reader = response.body.getReader();
+    }).then(res => {
+      const reader = res.body.getReader();
       const stream = new ReadableStream({
         start(controller) {
           function push() {
@@ -34,14 +33,52 @@ export class Covid19Service {
         }
       });
       return new Response(stream, { headers: { 'Content-Type': 'text/html' } });
-    }).then(res => {
-      return res.json();
-    }).then(res => {
-      return res.stat_by_country.slice(-5);
-    }).catch(err => {
+    }).then(
+      res => {
+        return res.json();
+      }
+    ).then(
+      res => {
+        return res.latest_stat_by_country[0];
+      }
+    ).catch(err => {
       console.log(err);
     });
-
     return data;
+  }
+
+  getCasesByCountry() {
+    return fetch('https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+        'x-rapidapi-key': 'fc9b700d8cmsh07c4965a20c35efp18bd1ajsn0bb779b30d62'
+      }
+    }).then(response => {
+      const reader = response.body.getReader();
+      const stream = new ReadableStream({
+        start(controller) {
+          function push() {
+            return reader.read().then(({ done, value }) => {
+              if (done) {
+                controller.close();
+                return;
+              }
+              controller.enqueue(value);
+              push();
+            });
+          }
+          push();
+        }
+      });
+      return new Response(stream, { headers: { 'Content-Type': 'text/html' } });
+    }).then( res => {
+      return res.json();
+    }).then( res => {
+      return res.countries_stat;
+    }
+    ).catch(err => {
+      console.log(err);
+    });
   }
 }
--- src/app/services/covid-19-helper.service.ts
+++ src/app/services/covid-19-helper.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CovidCases } from '../models/covid-cases';
+import { CovidCases } from '../interfaces/covid-cases';
 import { Covid19Service } from '../covid-19.service';
+import { AngularDelegate } from '@ionic/angular';
 
 
 @Injectable({
@@ -9,27 +10,19 @@ import { Covid19Service } from '../covid-19.service';
 
 export class Covid19HelperService {
 
-  private casesHistoric: CovidCases[] = [];
-  private casesTimeStamp;
-
   constructor(
     private covid19Service: Covid19Service
   ) { }
 
-  getCovid19CasesByCountrySpain(): Promise<CovidCases> {
-    if (this.casesHistoric && this.casesHistoric.length) {
-      return new Promise((resolve) => {
-        resolve(this.casesHistoric[0]);
-      });
-    } else {
-      return this.covid19Service.getCasesByCountrySpain().then(res => {
-        this.casesHistoric = res;
-        if (res && res.length) {
-          return this.casesHistoric[0];
-        } else {
-          return null;
-        }
-      });
-    }
+  async getCovid19CasesByCountrySpain() {
+    return this.covid19Service.getCasesByCountrySpain().then(res => {
+      return res;
+    });
+  }
+
+  getCasesByCountry() {
+    return this.covid19Service.getCasesByCountry().then(res => {
+      return res;
+    });
   }
 }
