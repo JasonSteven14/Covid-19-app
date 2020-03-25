61c7ac6ed7e272a9031c979b3ced7da603fef026
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-25 23:08:28 +0100

    Refactor and style home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ModuleWithComponentFactories } from '@angular/core';
 import { Covid19HelperService } from '../services/covid-19-helper.service';
-import { CovidCases } from '../models/covid-cases';
+import { CovidCases } from '../interfaces/covid-cases';
+import { interval, Observable,  } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -10,19 +14,41 @@ import { CovidCases } from '../models/covid-cases';
 })
 export class HomePage implements OnInit {
 
-  cases: CovidCases;
+  time = new Date().toUTCString();
+  public currentTime = this.time;
+  casesSpain: CovidCases;
+  casesCovid19: CovidCases;
+  titletl: string;
 
   constructor(
-    private covid19Helper: Covid19HelperService
-  ) { }
+    private covid19Helper: Covid19HelperService,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+   }
 
   ngOnInit() {
+
+    this.translate.get('TITLE').subscribe(res => {
+      this.titletl = res;
+    }
+    )
+    this.covid19Helper.getCovid19CasesByCountrySpain().then(res => {
+      return this.casesSpain = res;
+    });
+    this.covid19Helper.getCasesByCountry().then(res => {
+      this.casesCovid19 = res;
+    });
+  }
+
+  doRefresh(event: any) {
     this.covid19Helper.getCovid19CasesByCountrySpain().then(
       res => {
-        this.cases = res;
+        this.casesSpain = res;
+        event.target.complete();
       }
     );
   }
-  refreshCases() {
-  }
+
 }
